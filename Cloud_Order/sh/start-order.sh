6da72763eb72b99ac98e6d05a63e99603f5796a1
koujyungenn <513137767@qq.com>
2023-04-21 20:58:17 +0800

    更改脚本

--- Cloud_Order/sh/start-order.sh
+++ Cloud_Order/sh/start-order.sh
@@ -7,7 +7,7 @@ echo "containerId=$containerId,imageId=$imageId"
 docker stop $containerId
 docker rm $containerId
 docker rmi $imageId
-cp ../target/*.jar ../dockerfiles
-docker build -t $IMAGE_NAME ../dockerfiles/
+cp /home/kjg1/jenkins_output/Cloud_Order/target/*.jar /home/kjg1/jenkins_output/Cloud_Order/dockerfiles
+docker build -t $IMAGE_NAME /home/kjg1/jenkins_output/Cloud_Order/dockerfiles
 docker run -d --network=host -p 8001:8001 -e "ORDER_META=this is order meta" --add-host=kjg-pc:192.168.120.161 $IMAGE_NAME --name=test-order-env
 docker push $IMAGE_NAME
--- Cloud_Stock/sh/start-stock.sh
+++ Cloud_Stock/sh/start-stock.sh
@@ -7,7 +7,7 @@ echo "containerId=$containerId,imageId=$imageId"
 docker stop $containerId
 docker rm $containerId
 docker rmi $imageId
-cp ../target/*.jar ../dockerfiles
-docker build -t $IMAGE_NAME ../dockerfiles/
+cp /home/kjg1/jenkins_output/Cloud_Stock/target/*.jar /home/kjg1/jenkins_output/Cloud_Stock/dockerfiles
+docker build -t $IMAGE_NAME /home/kjg1/jenkins_output/Cloud_Stock/dockerfiles
 docker run -d --network=host -p 9001:9001 -e "STOCK_META=10086" --add-host=kjg-pc:192.168.120.161 $IMAGE_NAME --name=test-stock-env
 docker push $IMAGE_NAME
